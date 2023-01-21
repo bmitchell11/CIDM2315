3b7f51008aef3f58f6d1626ea00a86992f068be0
bmitchell11 <123218469+bmitchell11@users.noreply.github.com>
2023-01-20 20:58:31 -0500

    Add files via upload

---
+++ Homework.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        Console.WriteLine($"X = {x}, Y = {y}");
+        Console.WriteLine($"The value of Z is = {(4*x*x)+(3*y)}");
+        
+    }
+}
