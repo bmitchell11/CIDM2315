b6832b0a89a0d83b241ace6db23e5ef68edbb9d7
bmitchell11 <123218469+bmitchell11@users.noreply.github.com>
2023-01-27 18:30:42 -0500

    Add files via upload

---
+++ HW2_Bonus.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework2_Bonus;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Please input a year");
+        string input_year = Console.ReadLine();
+        int x = Convert.ToInt16(input_year);
+
+        if(x%4==0){
+            if(!(x%100==0)||(x%400==0)){
+                Console.WriteLine($"{input_year} is a Leap Year!");
+            }else{
+                Console.WriteLine($"{input_year} is not a Leap Year");
+        }
+        }else{
+            Console.WriteLine($"{input_year} is not a Leap Year");
+        }
+    }
+}
