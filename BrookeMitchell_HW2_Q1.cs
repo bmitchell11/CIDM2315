205e193f6ffeaeb9955b6da4f95841788892eaf3
bmitchell11 <123218469+bmitchell11@users.noreply.github.com>
2023-01-27 13:18:02 -0500

    Add files via upload

---
+++ BrookeMitchell_HW2_Q1.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework2;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+       Console.WriteLine("Please input a letter grade");
+        string input_letter_grade = Console.ReadLine();
+        if(input_letter_grade=="A"){
+            Console.WriteLine("GPA Point: 4");
+        }else if(input_letter_grade=="B"){
+            Console.WriteLine("GPA Point: 3");
+        }
+        else if(input_letter_grade=="C"){
+            Console.WriteLine("GPA Point: 2");
+        }
+        else if(input_letter_grade=="D"){
+            Console.WriteLine("GPA Point: 1");
+        }
+        else if(input_letter_grade=="F"){
+            Console.WriteLine("GPA Point: 0");
+        }else{
+            Console.WriteLine("Wrong Letter Grade!");
+        }
+        }
+
+    }
+
