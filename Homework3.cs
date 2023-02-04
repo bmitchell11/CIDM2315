b214e1cf9e3180248e961e0476438bc3689b06e1
bmitchell11 <123218469+bmitchell11@users.noreply.github.com>
2023-02-04 12:44:02 -0500

    Add files via upload

---
+++ Homework3.cs
@@ -0,0 +1,66 @@
+﻿namespace Homework3;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+        //Q1
+
+    Console.WriteLine("Input an integer");
+    int x = Convert.ToInt16(Console.ReadLine());
+    int y = 0;
+    for(int i = 1; i<=x; i++)
+    {
+        if(x%i==0){
+            y++;
+        }
+
+    }
+    if(y==2)
+    {
+        Console.WriteLine($"{x} is a prime number");
+    }else{
+        Console.WriteLine($"{x} is not a prime number");
+    }
+
+    
+
+
+
+
+        // Q2
+
+        Console.WriteLine("Assign an integer value to N:");
+        
+        int N = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row<N; row++){
+            for(int col = 0; col<N; col++){
+                Console.Write("#");
+            }
+            Console.WriteLine("");
+            }        
+    
+
+
+
+
+        
+    
+        //Q3
+
+        Console.WriteLine("Assign an integer value to N:");
+        int N = Convert.ToInt16(Console.ReadLine());
+        for(int row = 0; row<N; row++){
+            for(int col = 0; col<N; col++){
+                if(row>=col)
+                Console.Write("*");
+        }
+            Console.WriteLine("");    
+        }
+        
+
+        
+        
+}
+}
